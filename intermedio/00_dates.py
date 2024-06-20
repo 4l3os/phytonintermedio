52cf3afc88a3b343f39a76bedb059cd22d7158f4
4l3os <leoso4268@gmail.com>
2024-06-20 18:28:52 -0500

    Update 00_dates.py

--- intermedio/00_dates.py
+++ intermedio/00_dates.py
@@ -14,7 +14,7 @@ def print_date(date):
 
 print_date(now)
 
-year_2023 = datetime(2023, 1, 1, 3)
+year_2023 = datetime(2024, 1, 1, 3)
 
 
 print_date(year_2023)
@@ -48,3 +48,30 @@ current_date= date(2024,6,20 )
 print(current_date.year)
 print(current_date.month)
 print(current_date.day)
+
+current_date= date(current_date.year,current_date.month +1, current_date.day )
+
+print(current_date.month)
+
+diff = year_2023 - now
+print(diff)
+
+diff = year_2023.date() - current_date
+print(diff)
+
+print(year_2023.time())
+
+from datetime import timedelta
+
+start_timedelta=timedelta(200, 100, 100, weeks=10)
+end_timedelta=timedelta(300, 100, 100, weeks=13)
+
+print(end_timedelta - start_timedelta)
+print(end_timedelta + start_timedelta)
+
+
+
+
+
+
+
